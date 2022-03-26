c609a216baf3044de423df88f12c43125f0f2c65
maughan <rhys.maughan93@gmail.com>
2022-03-26 19:58:00 +0000

    moving routes out to router

---
+++ src/routes/substances/get.ts
@@ -0,0 +1,5 @@
+import router from '../router'
+
+router.get('/substances', (req, res) => {
+  res.send(200)
+})
--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,12 @@ import cors from 'cors'
 import http from 'http'
 import mongoose from 'mongoose'
 import { SubstanceModel } from './models/substance'
+import router from './routes/router'
 
 const MONGODB_URI = 'mongodb+srv://admin:459c5b425bbd50d788672eb948b0553b@peaches.ov13d.mongodb.net/dev?retryWrites=true&w=majority'
 
 const startServer = async () => {
   const app = express()
-  const router = Router()
   const port = 4000
 
   app.use(cors())
@@ -22,21 +22,21 @@ const startServer = async () => {
     throw new Error(err)
   }
 
-  router.get('/substances', async (req, res) => {
-    const substances = await SubstanceModel.find()
+  // router.get('/substances', async (req, res) => {
+  //   const substances = await SubstanceModel.find()
 
-    return void res.send(substances)
-  })
+  //   return void res.send(substances)
+  // })
 
-  router.get('/substance/:_id', async (req, res) => {
-    const { _id } = req.params
+  // router.get('/substance/:_id', async (req, res) => {
+  //   const { _id } = req.params
 
-    const substance = await SubstanceModel.findById(_id)
+  //   const substance = await SubstanceModel.findById(_id)
 
-    if (!substance) return void res.send(404)
+  //   if (!substance) return void res.send(404)
 
-    return void res.send(substance)
-  })
+  //   return void res.send(substance)
+  // })
 
   server.listen({ port }, () => {
     console.info(`🚀 Server running on port: ${port}`)
